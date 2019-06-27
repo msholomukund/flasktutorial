660430c222db000e9562228e8a1bade157a74e37
mkmkay <msholomukund@gmail.com>
2019-06-27 21:34:02 +0530

    syntax error fixed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app route('/')
+@app.route('/')
 def hello_world():
     return "W3C schools Hello World !"
-if __name__ = "main" :
+
+if __name__ == "__main__" :
     app.run(debug=True)
     
